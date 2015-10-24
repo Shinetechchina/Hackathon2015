202a2f0675f28ba0a167fe85bb69dbe7fb53c91c
liujavamail <liujavamail@gmail.com>
2015-10-24 21:38:35 +0800

    add bind

--- mobile/www/js/devices/bind-device.controller.js
+++ mobile/www/js/devices/bind-device.controller.js
@@ -1,15 +1,15 @@
 angular.module("devices", ['ionic', 'services'])
 .controller("BindDeviceController", function($scope, $state, DeviceCenter) {
-  $scope.saveDevice = function() {
-    DeviceCenter.getFamilyDevices();
-    // DeviceCenter.register({id: "1", name: "sdafdsf"}).then(
-    //   function(){
-    //     $state.go("familydevices")
-    //   },
-    //   function() {
-    //     DeviceCenter.getFamilyDevices();
-    //     console.log(DeviceCenter.token)
-    //   }
-    // )
+    var device = {
+      id: $scope.deviceId,
+      name: $scope.deviceName
+    }
+
+    $scope.saveDevice = function() {
+      DeviceCenter.register(device).then(function() {
+        $state.go("familydevices")
+      }, function() {
+         console.log("current error")
+      })
   }
 })
