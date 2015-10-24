bc308548068bf611e99035c2c80ae66768068c1c
zhengbb <zhengbb@zhuobei.net>
2015-10-24 20:57:01 +0800

    create branch zbb  for  many  updated file

---
+++ mobile/www/js/controller.js
@@ -0,0 +1,21 @@
+﻿angular.module('starter.controllers', ["ionic"])
+.controller('appCtrl', function ($scope, $state) {
+})
+.controller('devicesCtrl', function ($scope, $state) {
+})
+
+.controller('bindCtrl', function ($scope) {
+})
+
+.controller('familyDevicesCtrl', function ($scope) {
+})
+
+.controller('familyDevicesShowCtrl', function ($scope) {
+})
+.controller('dashboardCtrl', function ($scope) {
+})
+
+.controller('dashboardSettingCtrl', function ($scope) {
+})
+
+
