c5c5a165db29625c2cfde7c6c2c76c764c9e4dc4
zhengbb <zhengbb@zhuobei.net>
2015-10-24 21:13:10 +0800

    refact the archtect

--- mobile/www/js/liucontroller.js
+++ mobile/www/js/liucontroller.js
@@ -1,21 +1,20 @@
-﻿angular.module('starter.controllers', ["ionic"])
+﻿//刘民晗
+angular.module('starter.liucontrollers', ["ionic"])
+ //左侧菜单
 .controller('appCtrl', function ($scope, $state) {
 })
+ //未绑定设备的首页
 .controller('devicesCtrl', function ($scope, $state) {
 })
-
+//绑定树莓派
 .controller('bindCtrl', function ($scope) {
 })
-
-.controller('familyDevicesCtrl', function ($scope) {
-})
-
-.controller('familyDevicesShowCtrl', function ($scope) {
-})
+//我的树莓派
 .controller('dashboardCtrl', function ($scope) {
 })
-
+//定制智能家居
 .controller('dashboardSettingCtrl', function ($scope) {
 })
+
 
 
---
+++ mobile/www/js/zengcontroller.js
@@ -0,0 +1,10 @@
+﻿//曾智
+angular.module('starter.zengcontrollers', ["ionic"])
+ //绑定家居设备
+.controller('familyDevicesCtrl', function ($scope) {
+})
+
+//已绑定首页
+.controller('familyDevicesShowCtrl', function ($scope) {
+    alert("aaa");
+})
